30e1524320a7e982cb0e53aac5b7533c02593143
daniel <52224920+dezbrid@users.noreply.github.com>
2021-04-28 13:52:57 -0500

    save state in asyncstorage

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import 'react-native-gesture-handler';
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import Routes from './Routes';
 import { StoreContext, initialState, storeReducer } from './store';
 
 function App() {
   const [state, dispatch] = useReducer(storeReducer, initialState);
   console.log('GlobalState', state)
+  useEffect(() => {
+    async function setStateAsyncStorage() {
+      if (state) {
+        await AsyncStorage.setItem('state', JSON.stringify(state))
+      }
+    }
+    setStateAsyncStorage()
+
+  }, [state]);
   return (
     <StoreContext.Provider value={{ state, dispatch }}>
       <NavigationContainer>
