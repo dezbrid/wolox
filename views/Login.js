d2afc7023985a46e79223073d301b83c760abf0f
daniel <52224920+dezbrid@users.noreply.github.com>
2021-04-28 21:49:37 -0500

    regex y state

--- views/Login.js
+++ views/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
     SafeAreaView,
     View,
@@ -7,21 +7,42 @@ import {
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StoreContext } from '../store';
-import { LOGIN_ACTION } from '../Constant/actionType'
+import { LOGIN_ACTION } from '../Constant/actionType';
+import { onlyLetter, emailValidation } from '../Constant/regex';
 
 
 function Login() {
     const navigation = useNavigation();
     const storeContext = useContext(StoreContext);
-    const session_active=storeContext.state.session_active
+    const [signIn, setSignIn] = useState({
+        name: {
+            value: '',
+            error: false,
+            regex: onlyLetter,
+        },
+        last_name: {
+            value: '',
+            error: false,
+            regex: onlyLetter,
+        },
+        email: {
+            value: '',
+            error: false,
+            regex: emailValidation,
+        },
+        age: '',
+        terms: false
+    })
+    const session_active = storeContext.state.session_active;
+    const dispatch = storeContext.dispatch
     useEffect(() => {
-        if (session_active){
+        if (session_active) {
             navigation.navigate("Main")
         }
     }, [session_active])
     function onLogin() {
-        storeContext.dispatch({ type: LOGIN_ACTION, payload: true })
-       
+        dispatch({ type: LOGIN_ACTION, payload: true })
+
     }
     return (
         <SafeAreaView style={{ flex: 1 }}>
